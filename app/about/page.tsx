e2edf971fd20ae7ed3ce90c3c9301febd5262aea
Yuma Maeda <e235715@ie.u-ryukyu.ac.jp>
2025-08-25 15:27:33 +0900

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About us</h1>
+            <p>これは私たちのNext.jsアプリケーションについてのページです。</p>
+        </div>
+    )
+}
---
+++ app/hello-nextjs/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    const greeting = 'Hello';
+    return (
+        <div>
+            <h1>{greeting}, Next.js</h1>
+        </div>
+    )
+}
